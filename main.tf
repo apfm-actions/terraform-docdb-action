521a18362f711861c53ba0cc20c7662a5f6203ba
rickymaw <rickym@aplaceformom.com>
2020-05-14 15:41:59 -0700

    changed how variables are called. testing cw exports.

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ resource "aws_docdb_cluster" "docdb" {
   engine                          = "docdb"
   db_subnet_group_name            = aws_docdb_subnet_group.subnet_group.id
   db_cluster_parameter_group_name = var.parameter_group
-  vpc_security_group_ids          = "${split(",", var.security_group_ids)}"
-  #enabled_cloudwatch_logs_exports = "${split(",", var.cloudwatch_log_types)}"
+  vpc_security_group_ids          = split(",", var.security_group_ids)
+  enabled_cloudwatch_logs_exports = "" #"${split(",", var.cloudwatch_log_types)}"
   
-  master_username = "${replace(var.cluster_name,"-","_")}_admin"
+  master_username = replace(var.cluster_name,"-","_")_admin
   master_password = "supersecurepassword###123"  #"${data.credstash_secret.password.value}"
 
   preferred_backup_window = var.backup_window
@@ -31,9 +31,9 @@ resource "aws_docdb_cluster" "docdb" {
 
 resource "aws_docdb_cluster_instance" "instance" {
   count             = var.instance_count
-  identifier_prefix = "${aws_docdb_cluster.docdb.id}"
+  identifier_prefix = aws_docdb_cluster.docdb.id
   instance_class    = var.instance_class
-  cluster_identifier = "${aws_docdb_cluster.docdb.id}"
+  cluster_identifier = aws_docdb_cluster.docdb.id
 
   # tags = {
   #   project = "${var.project}"
@@ -44,7 +44,7 @@ resource "aws_docdb_cluster_instance" "instance" {
 
 resource "aws_docdb_subnet_group" "subnet_group" {
   name_prefix = var.cluster_name
-  subnet_ids  = "${split(",", var.subnet_id_private)}"
+  subnet_ids  = split(",", var.subnet_id_private)
 
   # tags = {
   #   project = "${var.project}"
